6b75f9eb1c70f7159dba4763ce6e9fe96f500838
RituRaj693 <45709450+RituRaj693@users.noreply.github.com>
2019-05-27 16:46:36 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,40 @@
+'''def add_two(a,b):
+    return(a+b)
+num1=int(input('enter the number :'))
+num2=int(input('enter the number :'))
+total=add_two(num1,num2)
+print(total)'''
+
+
+
+'''def lastchar(name):
+    return name[-1]
+char = input('enter the name :')
+last_chracter=lastcharritu(char)
+print(last_chracter)'''
+
+'''def lastchar(name):
+    return (name[-1])
+print(lastchar('ritu'))'''
+
+'''def odd_even(number):
+    if number%2 == 0:
+        return even
+    else:
+        return odd
+
+ritu=odd_even(num)
+print=(ritu)'''
+
+def greater_number(num1,num2):
+    if num1>num2:
+        return num1
+    else:
+        return num2
+numb1=int(input('enter the number1 :'))
+numb2=int(input('enter the number2 :'))
+largest = greater_number(numb1,numb2)
+print(largest)
+
+
+
