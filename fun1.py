a26ae9d10152a65ec096f41474b6de9047e8ac97
RituRaj693 <45709450+RituRaj693@users.noreply.github.com>
2019-05-28 19:18:43 +0530

    Add files via upload

---
+++ fun1.py
@@ -0,0 +1,23 @@
+
+
+'''def bigger(num1,num2):
+    if num1>num2:
+        return 'num1'
+    elif num1==num2:
+        return 'same'
+    else:
+        return 'num2'
+numb1 = int(input('enter the number:'))
+numb2 = int(input('enter the number:'))
+numb3 = (bigger(numb1,numb2))
+print(numb3)'''
+
+
+a = input('type string')
+word = a[::-1]
+if word ==a :
+    print('true')
+else:
+    print('false')
+
+
